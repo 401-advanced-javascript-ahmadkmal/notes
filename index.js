dca8d688891acf4e09b756f159512e661e66fef6
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-05-29 18:39:35 +0300

    install .env

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 'use strict';
 
 const Input = require('./lib/input.js');
-
+const mongoose = require('mongoose');
 const Note = require('./lib/note.js');
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const options = new Input();
 const note = new Note(options);
 // console.log(note);
